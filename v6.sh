ab195af23432238a227f48c779d95a5bcd403745
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-20 02:22:22 +0330

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -26,11 +26,11 @@ if [ "$choices" -eq 1 ]; then
 rctext='#!/bin/bash
 
 ip tunnel add 6to4tun_IR mode sit remote '"$ip_remote"' local '"$ipv4_address"'
-ip -6 addr add 2001:470:1f10:e1f::1/64 dev 6to4tun_IR
+ip -6 addr add fd51:7b73:0b36::1/64 dev 6to4tun_IR
 ip link set 6to4tun_IR mtu 1480
 ip link set 6to4tun_IR up
 # confige tunnele GRE6 ya IPIPv6 IR
-ip -6 tunnel add GRE6Tun_IR mode ip6gre remote 2001:470:1f10:e1f::2 local 2001:470:1f10:e1f::1
+ip -6 tunnel add GRE6Tun_IR mode ip6gre remote fd51:7b73:0b36::2 local fd51:7b73:0b36::1
 ip addr add 172.16.1.1/30 dev GRE6Tun_IR
 ip link set GRE6Tun_IR mtu 1436
 ip link set GRE6Tun_IR up
@@ -57,11 +57,11 @@ elif [ "$choices" -eq 2 ]; then
   read -p "enter Iran Ip : " ip_remote
   rctext='#!/bin/bash
 ip tunnel add 6to4tun_KH mode sit remote '"$ip_remote"' local '"$ipv4_address"'
-ip -6 addr add 2001:470:1f10:e1f::2/64 dev 6to4tun_KH
+ip -6 addr add fd51:7b73:0b36::2/64 dev 6to4tun_KH
 ip link set 6to4tun_KH mtu 1480
 ip link set 6to4tun_KH up
 
-ip -6 tunnel add GRE6Tun_KH mode ip6gre remote 2001:470:1f10:e1f::1 local 2001:470:1f10:e1f::2
+ip -6 tunnel add GRE6Tun_KH mode ip6gre remote fd51:7b73:0b36::1 local fd51:7b73:0b36::2
 ip addr add 172.16.1.2/30 dev GRE6Tun_KH
 ip link set GRE6Tun_KH mtu 1436
 ip link set GRE6Tun_KH up
@@ -107,8 +107,8 @@ echo    # move to a new line
 if [[ $yes_no =~ ^[Yy]$ ]] || [[ $yes_no =~ ^[Yy]es$ ]]; then
 	bash <(curl -Ls https://raw.githubusercontent.com/iPmartNetwork/3x-ui/main/install.sh)
 fi
-echo "IP Kharej: 2001:470:1f10:e1f::2"
-echo "IP Iran: 2001:470:1f10:e1f::1"
+echo "IP Kharej: fd51:7b73:0b36::2"
+echo "IP Iran: fd51:7b73:0b36::1"
 echo "
 ____________________________________________________________________________________
         ____                             _     _                                     
